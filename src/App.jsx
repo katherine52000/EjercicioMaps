c26c60f73adf64878f3975dcd84561817cb361dc
katherine52000 <kath52000@outlook.es>
2022-07-27 19:21:31 -0500

    ejercicio-maps-react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Persons from './componentes/Persons/Persons';
+
+
+const App = () => {
+  return (
+    <div>
+      <Persons/>
+    </div>
+  )
+}
+
+export default App
